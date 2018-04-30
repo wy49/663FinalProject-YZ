0b4f94793c53d3b999e6c53fe31d613fa2835c2f
Elaine Zhang <31319982+Beceelaine@users.noreply.github.com>
2018-04-30 16:24:20 -0400

    I deleted a file and added a new file.

--- IBP/likelihood_old.py
+++ IBP/likelihood_old.py
@@ -1,23 +1,24 @@
-from numba import jit
+
+from __future__ import division
 import numpy as np
+import numpy.linalg
+import math
 
+#compute likelihood
 np.random.seed(123)
-
-@jit
-
-def full_X(Z,X,s_x,s_a):
+def full_X_old(Z,X,s_x,s_a):
     D = X.shape[1]
     N = Z.shape[0]
     K = Z.shape[1]
     """The constant part"""
-    zz = Z.T@Z+np.diag([(s_x**2)/(s_a**2)]*K) #zz -- K*K
+    zz = np.dot(Z.T,Z)+np.diag([(s_x**2)/(s_a**2)]*K) #zz -- K*K
     determ = np.linalg.det(zz)
     log_const = 0.5*N*D*np.log(2*np.pi)+(N-K)*D*np.log(s_x)+K*D*np.log(s_a)+0.5*D*np.log(determ)
     log_const = -log_const
     """The exponential part"""
     L = np.linalg.cholesky(zz)
     inv_L = np.linalg.inv(L)
-    ii =  np.eye(N)-(Z @ inv_L.T @ inv_L @ Z.T)
-    tr =  np.trace(X.T @ ii @X)
+    ii =  np.eye(N)-np.dot(np.dot(np.dot(Z , inv_L.T), inv_L) , Z.T)
+    tr =  np.trace(np.dot(np.dot(X.T , ii) ,X))
     expon = -tr/(2*s_x**2)
     return(log_const+expon)
